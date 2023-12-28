03580b48c65b95b6005d640f35148afd932118b5
hthere21 <114700489+hthere21@users.noreply.github.com>
2023-12-27 22:41:38 -0800

    30min

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Project_.Net_6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
